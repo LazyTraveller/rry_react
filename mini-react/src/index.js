fed0670cfb69adce147f68d943ee94b164951213
heweizhi <heweizhi@guanmai.cn>
2022-04-13 10:49:20 +0800

    调度

--- mini-react/src/index.js
+++ mini-react/src/index.js
@@ -34,7 +34,7 @@ function createTextElement(text) {
  * @param {*} fiber 
  * @returns 
  */
-function creatDom(fiber) {
+function createDom(fiber) {
   const dom = fiber.type === 'TEXT_ELEMENT' ? document.createTextNode('') : document.createElement(fiber.type)
 
   updateDom(dom, {}, fiber.props)
@@ -89,3 +89,178 @@ function updateDom(dom, prevProps, nextProps) {
     })
 
 }
+
+
+
+
+// commit 阶段
+function commitRoot() {
+  deletions.forEach(commitWork)
+  commitWork(wipRoot.child)
+  currentRoot = wipRoot
+  wipRoot = null
+  
+}
+
+// 操作真实dom
+/**
+ * 
+ * @param {*} fiber 
+ * @returns 
+ */
+function commitWork(fiber) {
+  if (!fiber) {
+    return
+  }
+  const domParent = fiber.parent.dom
+  if (fiber.effectTag === 'PLACEMENT' && fiber.dom !== null) {
+    domParent.appendChild(fiber.dom)
+  } else if (fiber.effectTag === 'UPDATE' && fiber.dom !== null) {
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props)
+  } else if (fiber.effectTag === 'DELETION') {
+    domParent.removeChild(fiber.dom)
+  }
+
+  commitWork(fiber.child)
+  commitWork(fiber.sibling)
+}
+
+let nextUnitOfWork = null
+let currentRoot = null
+let wipRoot = null
+let deletions = null
+
+// 渲染开始的入口
+/**
+ * 
+ * @param {*} element 
+ * @param {*} container 
+ */
+function render(element, container) {
+  wipRoot = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+    alternate: currentRoot,
+  }
+  deletions = []
+  nextUnitOfWork = wipRoot
+}
+
+// 调度函数
+function workLoop(deadline) {
+  let shouldYield = false
+  while (nextUnitOfWork && !shouldYield) {
+    // render阶段
+    nextUnitOfWork = performUnitOfWork(nextUnitOfWork)
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+
+  if (!nextUnitOfWork && wipRoot) {
+    commitRoot()
+  }
+
+  requestIdleCallback(workLoop)
+}
+
+requestIdleCallback(workLoop)
+
+// render阶段
+function performUnitOfWork(fiber) {
+  if (fiber.dom) {
+    fiber.dom = createDom(fiber)
+  }
+
+  const elements = fiber.props.children
+  reconcileChildren(fiber, elements)
+
+  if (fiber.child) {
+    return fiber.child
+  }
+
+  let nextFiber = fiber
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling
+    }
+    nextFiber = nextFiber.parent
+  }
+}
+
+// 节点调度
+function reconcileChildren(wipFiber, elements) {
+  let index = 0
+  let oldFiber = wipFiber.alternate && wipFiber.alternate.child
+  let prevSibling = null
+  while (index < elements.length || (oldFiber !== null && oldFiber !== undefined)) {
+    const element = elements[index]
+    let newFiber = null
+    const sameType = oldFiber && element && element.type === oldFiber.type
+
+    if (sameType) {
+      newFiber = {
+        type: oldFiber.type,
+        props: element.props,
+        dom: oldFiber.dom,
+        parent: wipFiber,
+        alternate: oldFiber,
+        effectTag: 'UPDATE'
+      }
+    }
+
+    if (element && !sameType) {
+      newFiber = {
+        type: element.type,
+        props: element.props,
+        dom: null,
+        parent: wipFiber,
+        alternate: null,
+        effectTag: 'PLACEMENT',
+      }
+    }
+
+    if (oldFiber && !sameType) {
+      oldFiber.effectTag = "DELETION";
+      deletions.push(oldFiber);
+    }
+
+    if (oldFiber) {
+      oldFiber = oldFiber.sibling
+    }
+
+    if (index === 0) {
+      wipFiber.child = newFiber
+    } else if (element) {
+      prevSibling.sibling = newFiber
+    }
+
+    prevSibling = newFiber
+    index++
+
+  }
+}
+
+const React = {
+  creatElement,
+  render,
+}
+
+const container = document.getElementById('root')
+
+const updateValue = (e) => {
+  reRender(e.target.value)
+}
+
+const reRender = (value) => {
+  const element = (
+    <div>
+      <input onInput={updateValue} value={value}></input>
+      <h2>hello, {value}</h2>
+    </div>
+  )
+
+  React.render(element, container)
+}
+
+reRender('World')
